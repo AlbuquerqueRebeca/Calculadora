5a345259a6826c820f6ccb634227dee236bcf9bd
AlbuquerqueRebeca <becalbuquerque_23@hotmail.com>
2026-02-03 19:35:18 -0300

    adicionando a opçoes de operaçao

--- calculadora/calculadora.go
+++ calculadora/calculadora.go
@@ -12,4 +12,10 @@ func main() {
 	fmt.Println("Digite o número:")
 	fmt.Scan(&y)
 
+	fmt.Println("Escolha a operação: ")
+	fmt.Println("1 - soma")
+	fmt.Println("2 - subtracao")
+	fmt.Println("3 - multiplicacao")
+	fmt.Println("4 - divisao")
+
 }
