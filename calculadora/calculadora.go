4418c8b5ccb35045d9f84ca71d7ef5fb9a80cb30
AlbuquerqueRebeca <becalbuquerque_23@hotmail.com>
2026-02-03 19:32:45 -0300

    adicionando mensagem

--- calculadora/calculadora.go
+++ calculadora/calculadora.go
@@ -1,8 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 
 	var x, y int
 	var opcoes int
+
+	fmt.Println("Digite o número: ")
+	fmt.Scan(&x)
+	fmt.Println("Digite o número:")
+	fmt.Scan(&y)
 
 }
