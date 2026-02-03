9a162d9c59609d632fa572bcfe07d4540a9d1105
AlbuquerqueRebeca <becalbuquerque_23@hotmail.com>
2026-02-03 19:39:56 -0300

    adicionando switch case para fazer uma operação por vez

--- calculadora/calculadora.go
+++ calculadora/calculadora.go
@@ -13,9 +13,23 @@ func main() {
 	fmt.Scan(&y)
 
 	fmt.Println("Escolha a operação: ")
+	fmt.Scan(&opecoes)
 	fmt.Println("1 - soma")
 	fmt.Println("2 - subtracao")
 	fmt.Println("3 - multiplicacao")
 	fmt.Println("4 - divisao")
+
+	switch opcoes{
+	case 1:
+		fmt.Println(soma(x, y ))
+	case 2:
+		fmt.Println(subtracao(x y ))
+	case 3:
+		fmt.Println(multiplicacao(x y))
+	case 4:
+		fmt.Println(divisao(x, y))
+	default:
+		fmt.Println("Opçao inválida")	
+	}
 
 }
