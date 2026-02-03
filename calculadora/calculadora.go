8321c603d2312306c3beb3d65e115921631af269
AlbuquerqueRebeca <becalbuquerque_23@hotmail.com>
2026-02-03 19:45:57 -0300

    adicionando ajustes

--- calculadora/calculadora.go
+++ calculadora/calculadora.go
@@ -19,34 +19,34 @@ func main() {
 	fmt.Println("3 - multiplicacao")
 	fmt.Println("4 - divisao")
 
-	switch opcoes{
+	switch opcoes {
 	case 1:
-		fmt.Println(soma(x, y ))
+		fmt.Println(soma(x, y))
 	case 2:
-		fmt.Println(subtracao(x y ))
+		fmt.Println(subtracao(x, y))
 	case 3:
-		fmt.Println(multiplicacao(x y))
+		fmt.Println(multiplicacao(x, y))
 	case 4:
 		fmt.Println(divisao(x, y))
 	default:
-		fmt.Println("Opçao inválida")	
+		fmt.Println("Opçao inválida")
 	}
 
 }
 
-func soma(x int, y int)int{
-	retunr x + y
+func soma(x int, y int) int {
+	return x + y
 }
 
-func subtracao(x int, y int)int{
+func subtracao(x int, y int) int {
 	return x - y
 }
 
-func multiplicacao(x int, y int)int{
-	return x *y
+func multiplicacao(x int, y int) int {
+	return x * y
 }
 
-func divisao(x int, y int)int{
+func divisao(x int, y int) int {
 	if y == 0 {
 		fmt.Println("Erro: divisão por zero")
 	}
